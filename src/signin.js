f5abd6a1e1d2ff24edf09248aafc0ca5ecf7ad40
sansSpoon <sans.spoon@gmail.com>
2018-05-04 17:50:29 +1000

    adding fetch, maybe wrong place

--- src/signin.js
+++ src/signin.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 
+function logResponse(response) {
+	console.log(response.headers.get('Content-Type'));
+	console.log(response.headers.get('Date'));
+	console.log(response.status);
+	console.log(response.statusText);
+	console.log(response.type);
+	console.log(response.url);
+	return response;
+}
+
+function json(response) {
+  return response.json()
+}
+
+function status(response) {
+	if (response.ok) {
+		return Promise.resolve(response)
+	} else {
+		return Promise.reject(new Error(response.statusText))
+	}
+}
+
 
 class Signin extends Component {
 	constructor(props){
@@ -21,6 +43,32 @@ class Signin extends Component {
 
 	handleSubmit(event) {
 		alert(`Submitted ${this.state.email} and ${this.state.password}`);
+		
+		const apiBaseUri = "http://localhost:3000/auth/signin",
+		payload = {
+			"email": this.state.email,
+			"password": this.state.password
+		},
+		initGet = {
+			body: payload,
+			method: 'POST',
+			mode: 'cors',
+			cache: 'default',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+		
+		fetch(apiBaseUri, initGet)
+			.then(logResponse)
+			.then(status)
+			.then(json)
+			.then(data => {
+				console.log(data);
+			}).catch(error => {
+				console.log('There has been a problem with the fetch operation: ', error.message);
+			});
+		
 		event.preventDefault();
 	}
 
@@ -40,3 +88,7 @@ class Signin extends Component {
 }
 
 export default Signin;
+
+/*
+@example curl -i -XPOST '<host>:<port>/auth/signin' -d '{"email":"foo@foo.com", "password":"notARealPassword123!@#"}' -H 'Content-Type: application/json'
+*/
