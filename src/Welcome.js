ce69750349b8d4038070c537b90965ae8389809e
sansSpoon <sans.spoon@gmail.com>
2018-05-11 18:20:22 +1000

    Combined sign in/up form

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 
-import SignUp from './SignUp';
-import SignIn from './SignIn';
+//import SignUp from './SignUp';
+//import SignIn from './SignIn';
 
 class Welcome extends Component {
 
 	constructor(props) {
+		
 		super(props);
 		this.state = {
-			signUp: false,
+			email: '',
+			password: '',
+		}
+		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	
+	handleChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+		this.setState({[name]: value});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		
+		const target = event.target;
+		const name = target.name;
+		
+		if (name === 'signIn') {
+			console.log(`Submitted ${this.state.email} and ${this.state.password} from Sign In`);
+		} else {
+			console.log(`Submitted ${this.state.email} and ${this.state.password} from Sign Up`);
 		}
 	}
 
 	render() {
-		if (this.state.signUp) {
-			return <SignUp />
-		} else {
-			return <SignIn />
-		}
+		return(
+			<div>
+				<p>Welcome</p>
+				<form>
+					<label htmlFor="email">Email</label>
+					<input id="email" name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+					<label htmlFor="password">Password</label>
+					<input id="password" name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+					<input type="submit" name="signIn" value="Sign In" onClick={this.handleSubmit} />
+					<input type="button" name="signUp" value="Sign Up" onClick={this.handleSubmit} />
+				</form>
+			</div>
+		);
 	}
 	
 }
 
-export default Welcome;
+export default Welcome; 
