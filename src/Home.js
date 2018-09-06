2a7c9264785557f44a55ad027ee2f32aea4c7966
sansSpoon <sans.spoon@gmail.com>
2018-09-06 21:47:14 +1000

    cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import './home.css';
 
 export default function WelcomeContainer() {
 	return (
 		<AuthContext.Consumer>
 			{footastic => (
-				<div>
+				<div className="bg">
 					<p>Email: {footastic.authUser}</p>
 					<p>Token: {footastic.token}</p>
 					<p>Authenticated: Duh, {footastic.authenticated.toString()}</p>
